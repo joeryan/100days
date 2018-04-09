4cce999be2124bde8990e4ddd1bd2455a3b2bb6b
Joe Ryan <joe@clanryan.us>
2018-04-09 00:15:29 -0400

    created check_in and check_out functionality

---
+++ hotel.py
@@ -0,0 +1,24 @@
+import pytest
+
+class Hotel:
+  _guests = []
+
+  def check_in(self, guest_name):
+    self._guests.append(guest_name)
+
+  def check_out(self, guest_name):
+    self._guests.remove(guest_name)
+
+  def guests(self):
+    return self._guests
+
+def test_check_in_a_guest():
+  hotel = Hotel()
+  hotel.check_in('Bob Barker')
+  assert(('Bob Barker' in hotel.guests()) == True)
+
+def test_check_out_a_guest():
+  hotel = Hotel()
+  hotel.check_in('Bob Barker')
+  hotel.check_out('Bob Barker')
+  assert(('Bob Barker' in hotel.guests()) == False)
