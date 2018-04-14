64e0189b4de7353692671930446578e758262080
Joe Ryan <joe@clanryan.us>
2018-04-13 22:05:31 -0400

    created initial template for cybrary.py
    Initial template of required functionality for journeyman exercises.

---
+++ cybrary.py
@@ -0,0 +1,17 @@
+# cybrary1.py
+# exercise 1: practical applications in python
+# adapted and expanded from cybrary.it video course 
+# Python for Security Professionals at www.cybrary.it/course/python
+# used to improve pytest understanding and overall python knowledge
+
+# display a menu of options to choose from
+
+# 1. determine if the input is odd or even
+
+# 2. return the sum of two input values
+
+# 3. given a list of integers, determine how many are even
+
+# 4. answer the input string backwards
+
+# 5. determine if input string is a palindrome
